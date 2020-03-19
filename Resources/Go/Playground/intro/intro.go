6ec819e59fb286fd1fcc40862d4efeca6ce7d057
fluckmic <fmichael@student.ethz.ch>
2020-03-19 17:41:13 +0100

    added some go stuff

---
+++ Resources/Go/Playground/intro/intro.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+import "MaThe/Resources/Go/Playground/intro/sub"
+
+func main() {
+	fmt.Printf("Hello World.\n")
+}
---
+++ Resources/Go/Playground/intro/sub/sub.go
@@ -0,0 +1,9 @@
+package sub
+
+func Visible_Addition(a, b int)(int){
+  return invisible_addition(a,b)
+}
+
+func invisible_addition(a, b int)(int){
+  return a + b
+}
